afaa04cda5ef5f17da65c92f1e31852bee62736a
evillazana27 <edinson.villazana@phinxlab.com>
2021-11-11 23:40:52 -0400

    add header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import TopBar from "./TopBar/TopBar";
 
-export default function Header(props) { 
-    return (
-        <div className="header">
-            <TopBar/>
-            <div>menu</div>
-        </div >
-    )
+export default function Header(props) {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <TopBar />
+      </div>
+    </header>
+  );
 }
--- components/Header/TopBar/TopBar.tsx
+++ components/Header/TopBar/TopBar.tsx
@@ -1,29 +1,52 @@
-
 export default function TopBar(props) {
-    return (
-        <div className="top-bar">
-            <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'space-between' }}>
-                <div syle={{ width: '100px' }}>
-                    <Logo></Logo>
-                </div>
-                <div syle={{ width: '100px' }}>
-                    <Seach />
-                </div>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <Logo></Logo>
+      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      
+      </nav>
+      <Seach />
+    </div>
+  );
 }
 
-
 function Logo() {
-    return (
-        <div> aca creamos una imagen</div>
-    )
+  return (
+    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="164"
+        height="32"
+        viewBox="0 0 164 32"
+        fill="none"
+      >
+        <path
+          fill="#0F172A"
+          d="M41.793 0v31.022h-7.136V9.439l-13.693 16.62h-.354L6.913 9.438v21.583H0V0h7.763l13.202 16.4L34.167 0h7.626zM82.48 25.083v5.939H50.322V0h31.713v5.938H57.414v6.382h20.562v5.939H57.414v6.827l25.066-.003zm33.877 5.939l-8.964-10.548H96.108v10.548h-7.092V0h21.544c7.626 0 13.381 3.501 13.381 10.237 0 5.273-3.525 8.553-8.699 9.75l9.635 11.035h-8.52zm-6.199-16.397c3.746 0 6.513-.888 6.513-4.343 0-3.456-2.763-4.344-6.513-4.344H96.06v8.69l14.098-.003zM163.855 0v16.4c0 9.307-6.2 15.6-17.528 15.6-11.33 0-17.529-6.293-17.529-15.6V0h7.136v15.865c0 6.205 3.658 9.925 10.393 9.925 6.734 0 10.392-3.723 10.392-9.925V0h7.136z"
+        ></path>
+      </svg>
+      <span className="ml-3 text-xl md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+        Eccommerce
+      </span>
+    </a>
+  );
 }
 
 function Seach() {
-    return (
-        <div> aca creamos un search</div>
-    )
+  return (
+    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+      Button
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-4 h-4 ml-1"
+        viewBox="0 0 24 24"
+      >
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  );
 }
