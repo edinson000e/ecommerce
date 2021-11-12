20b1468855d53c5b61f99698d4c57b4bf943e289
evillazana27 <edinson.villazana@phinxlab.com>
2021-11-11 23:51:33 -0400

    add icon shopping

--- components/Header/TopBar/TopBar.tsx
+++ components/Header/TopBar/TopBar.tsx
@@ -34,19 +34,19 @@ function Logo() {
 
 function Seach() {
   return (
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-      Button
-      <svg
-        fill="none"
-        stroke="currentColor"
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
-        className="w-4 h-4 ml-1"
-        viewBox="0 0 24 24"
-      >
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
+        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+      />
+    </svg>
   );
 }
