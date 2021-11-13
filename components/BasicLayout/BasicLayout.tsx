ca7713f81f4271aead79d525f4ae1f6df894af6e
evillazana27 <edinson.villazana@phinxlab.com>
2021-11-13 01:01:49 -0400

    ADD HEAD

--- components/BasicLayout/BasicLayout.tsx
+++ components/BasicLayout/BasicLayout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Head from "next/head";
 import Header from "../Header";
 
 export default function BasicLayout(props) {
   const { children } = props;
   return (
     <div className="home">
+      <Head>
+        <title>Eccommerce</title>
+        <meta name="description" content="Eccommerce" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <div className="container mx-auto px-2">{children}</div>
     </div>
