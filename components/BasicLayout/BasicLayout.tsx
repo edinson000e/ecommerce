7f3a5cb12b092a2e7c6d3fa15c5304fb023ff844
evillazana27 <edinson.villazana@phinxlab.com>
2021-11-12 23:43:22 -0400

    modify

--- components/BasicLayout/BasicLayout.tsx
+++ components/BasicLayout/BasicLayout.tsx
@@ -6,7 +6,7 @@ export default function BasicLayout(props) {
   return (
     <div className="home">
       <Header />
-      <div className="container mx-auto px-4">{children}</div>
+      <div className="container mx-auto px-2">{children}</div>
     </div>
   );
 }
